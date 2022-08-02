59bbff5d096322d28abe80010d9ec6fdd07cf462
entest <hai@entest.io>
2022-08-02 08:21:01 +0000

    first

--- lib/assume-role-stack.ts
+++ lib/assume-role-stack.ts
@@ -1,16 +1,39 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, aws_iam, CfnOutput } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Effect } from "aws-cdk-lib/aws-iam";
+
+interface AssumeRoleProps extends StackProps {
+  account_id_guest: string;
+}
 
 export class AssumeRoleStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: AssumeRoleProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const role = new aws_iam.Role(this, "AssumedRoleByGuestAccount", {
+      roleName: "AssumedRoleByGuestAccount",
+      assumedBy: new aws_iam.AccountPrincipal(props.account_id_guest),
+      description: "enable a guest account to access s3",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AssumeRoleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    role.addToPolicy(
+      new aws_iam.PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["s3:*"],
+        resources: ["*"],
+      })
+    );
+
+    role.addToPolicy(
+      new aws_iam.PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["sts:AssumeRole"],
+        resources: ["*"],
+      })
+    );
+
+    new CfnOutput(this, "RoleArnOutput", {
+      value: role.roleArn,
+    });
   }
 }
