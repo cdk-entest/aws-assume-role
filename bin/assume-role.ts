9b4bd09ab47351892114f097e13410d73473cc97
entest <hai@entest.io>
2022-08-02 08:22:27 +0000

    rm account id

--- bin/assume-role.ts
+++ bin/assume-role.ts
@@ -4,5 +4,5 @@ import { AssumeRoleStack } from "../lib/assume-role-stack";
 
 const app = new cdk.App();
 new AssumeRoleStack(app, "AssumeRoleStack", {
-  account_id_guest: "610770234379",
+  account_id_guest: "",
 });
